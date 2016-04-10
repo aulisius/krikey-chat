6d2940d0896931b548948ebb153d389c4c75b6a1
faizaanceg <aulisius7@gmail.com>
2016-04-10 19:31:05 +0530

    Initial

---
+++ index.js
@@ -0,0 +1,110 @@
+'use strict'
+
+const express = require('express'),
+    debug = require('debug')('avar:server'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser')
+    
+const app = express()
+
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+const server = require('http').Server(app),
+    io = require('socket.io')(server)
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+});
+
+app.get('/chat', (req, res) => {
+    res.sendFile(__dirname + '/public/chat.html')
+})
+
+io.on('connection', socket => {
+
+    socket.on('Message.Send', user => {
+        console.log(user)
+        socket.broadcast.emit('Message.Receive', user)
+    })
+    
+    socket.on('Channel.Quit', user => {
+        console.log(user)
+        socket.broadcast.emit('Channel.Quit', user.curr)
+    })
+    
+    socket.on('Channel.Join', user => {
+        console.log(user)
+        socket.broadcast.emit('Channel.Enter', user.curr)
+    })
+    
+    socket.on('Nick.Changed', user => {
+        console.log(user)
+        socket.broadcast.emit('Nick.Changed', user.curr)
+    })
+    
+})
+
+/**
+ * Sets up the server and express app with error handlers
+ */
+
+const normalizePort = val => {
+    let port = parseInt(val, 10)
+
+    if (isNaN(port))
+        return val
+    else if (port >= 0)
+        return port
+    else
+        return false
+}
+
+const onError = error => {
+    if (error.syscall !== 'listen')
+        throw error
+
+    let bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port
+
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges')
+            process.exit(1)
+            break
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use')
+            process.exit(1)
+            break
+        default:
+            throw error
+    }
+}
+
+const onListening = () => {
+    let addr = server.address()
+    let bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port
+    debug('Listening on ' + bind)
+}
+
+let port = normalizePort(process.env.PORT || '8080')
+app.set('port', port)
+
+server.listen(port)
+server.on('error', onError)
+server.on('listening', onListening)
+
+app.use((err, req, res, next) => {
+    console.log(err + ' ' + err.message)
+})
+
---
+++ public/js/main.js
@@ -0,0 +1,162 @@
+$(function() {
+    $('.button-collapse').sideNav()
+    $('.modal-trigger').leanModal()
+});
+
+var socket = io()
+var currentChannel = "general"
+var previousChannel = "general"
+
+var currentNick = "Anonymous"
+var previousNick = "Anonymous"
+
+function getUser() {
+    var user = {}
+    user.prev = {
+        nick: previousNick,
+        channel: previousChannel
+    }
+    user.curr = {
+        nick: currentNick,
+        channel: currentChannel
+    }
+    return user
+}
+
+function makeNewMsg(dir, color, data) {
+    return $('<li>')
+        .append(
+        $('<div>').attr('class', 'row')
+            .append(
+            $('<div>').attr('class', 'col s6 m6 ' + dir)
+                .append(
+                $('<div>').attr('class', 'card ' + color)
+                    .append(
+                    $('<div>').attr('class', 'card-content white-text')
+                        .append(
+                        $('<span>').attr('class', 'card-title').text(data.info.nick)
+                        )
+                        .append(
+                        $('<p>').text(data.msg)
+                        )
+                    )
+                )
+            )
+        )
+}
+
+function gotoTop() {
+    window.scrollTo(0, 0)
+}
+
+function changeChannel() {
+    console.log('Changing channel...')
+    var channel = $('#channel').val()
+    $('#channel').trigger('autoresize')
+    $('#m').trigger('autoresize')
+
+    channel = channel.replace('/\s+/g', '')
+    if (channel.length === 0) return
+
+    if (currentChannel === channel) {
+        return
+    } else {
+        previousChannel = currentChannel
+        currentChannel = channel
+        notifyChannelQuit()
+        notifyChannelJoin()
+    }
+}
+
+function changeNick() {
+    console.log('Changing nick...')
+
+    var nick = $('#nick').val()
+    $('#nick').trigger('autoresize')
+    $('#m').trigger('autoresize')
+
+    nick = nick.replace('/\s+/g', '')
+    if (nick.length === 0) return
+
+    if (currentNick === nick) {
+        return
+    } else {
+        previousNick = currentNick
+        currentNick = nick
+        notifyNameChange()
+        Materialize.updateTextFields()
+    }
+}
+
+function notifyNameChange() {
+    socket.emit('Nick.Changed', getUser())
+}
+
+function notifyChannelQuit() {
+    socket.emit('Channel.Quit', getUser())
+}
+
+function notifyChannelJoin() {
+    socket.emit('Channel.Join', getUser())
+}
+
+$('#changeChannel').click(function() {
+    $('#channels').openModal()
+})
+
+
+$('#changeNick').click(function() {
+    $('#nicks').openModal()
+})
+
+$('#msg').submit(function() {
+
+    var data = {}
+    data.msg = $('#m').val()
+    data.info = getUser().curr
+    $('#messages').append(makeNewMsg('right', 'light-blue', data))
+    $('#messages').append($('<br>'))
+
+    window.scrollTo(0, document.body.scrollHeight)
+
+    socket.emit('Message.Send', data)
+    $('#m').val('')
+    $('#m').trigger('autoresize')
+    return false
+})
+
+socket.on('Message.Receive', function(user) {
+    if (user.info.channel === currentChannel) {
+        $('#messages').append(makeNewMsg('left', 'orange', user))
+        $('#messages').append(
+            $('<li>').append(
+                $('<div>').attr('class', 'divider')
+            )
+        )
+    }
+})
+
+
+socket.on('Channel.Quit', function(nick) {
+    //addNewNotif
+    console.log(nick + ' quit this channel')
+
+})
+
+socket.on('Channel.Join', function(nick) {
+    //addNewNotif
+    console.log(nick + ' joined this channel')
+
+})
+
+socket.on('Nick.Changed', function(nick) {
+    //addNewNotif
+    console.log(nick + ' joined this channel')
+
+})
+
+socket.on('Channel.Enter', function(nick) {
+    //addNewNotif
+    console.log(nick + ' joined this channel')
+
+})
